b1d1ff43fc3b93c57163c5970264d8c96cabe2c3
beytullahdanis <beytullahdanis19@gmail.com>
2024-12-31 00:12:32 +0300

    client update

--- src/socket.js
+++ src/socket.js
@@ -4,7 +4,7 @@ const SOCKET_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002';
 
 const socket = io(SOCKET_URL, {
   autoConnect: false,
-  transports: ['websocket'],
+  transports: ['polling', 'websocket'],
   withCredentials: true,
   reconnection: true,
   reconnectionAttempts: 5,
