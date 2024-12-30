f10e033a1f2a3c53676cd50bdd9174de8f229162
beytullahdanis <beytullahdanis19@gmail.com>
2024-12-30 22:29:29 +0300

    update socket

--- src/socket.js
+++ src/socket.js
@@ -4,8 +4,12 @@ const SOCKET_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002';
 
 const socket = io(SOCKET_URL, {
   autoConnect: false,
-  transports: ['polling', 'websocket'],
-  withCredentials: true
+  transports: ['websocket', 'polling'],
+  withCredentials: true,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  timeout: 60000
 });
 
 export default socket; 
