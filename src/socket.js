981fe2a645311e18bc0bb03aec44099a1d7e706a
beytullahdanis <beytullahdanis19@gmail.com>
2024-12-31 01:13:51 +0300

    console log added

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002';
-
+console.log(SOCKET_URL);
 const socket = io(SOCKET_URL, {
   autoConnect: false,
   transports: ['polling', 'websocket'],
