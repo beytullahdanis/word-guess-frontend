1c7ffd76357645152741dd435225240dec4c6fe9
beytullahdanis <beytullahdanis19@gmail.com>
2024-12-30 23:29:24 +0300

    revert socket changes

--- src/socket.js
+++ src/socket.js
@@ -5,27 +5,11 @@ const SOCKET_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002';
 const socket = io(SOCKET_URL, {
   autoConnect: false,
   transports: ['polling', 'websocket'],
-  withCredentials: false,
+  withCredentials: true,
   reconnection: true,
-  reconnectionAttempts: 10,
+  reconnectionAttempts: 5,
   reconnectionDelay: 1000,
-  reconnectionDelayMax: 5000,
-  timeout: 60000,
-  forceNew: true,
-  path: '/socket.io'
-});
-
-// Bağlantı durumunu izle
-socket.on('connect', () => {
-  console.log('Socket.IO bağlantısı başarılı');
-});
-
-socket.on('connect_error', (error) => {
-  console.error('Socket.IO bağlantı hatası:', error);
-});
-
-socket.on('disconnect', (reason) => {
-  console.log('Socket.IO bağlantısı kesildi:', reason);
+  timeout: 60000
 });
 
 export default socket; 
